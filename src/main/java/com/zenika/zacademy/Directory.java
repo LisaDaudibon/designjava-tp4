2bfedd9982fa17586442c270c9760261bfba8cf6
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-17 10:51:33 +0100

    :recycle: LD refactoring the directory method

--- src/main/java/com/zenika/zacademy/Directory.java
+++ src/main/java/com/zenika/zacademy/Directory.java
@@ -3,22 +3,25 @@ package com.zenika.zacademy;
 import java.util.ArrayList;
 
 public class Directory {
-    private ArrayList<Person> persons = new ArrayList<>();
+    public ArrayList<Person> persons = new ArrayList<>();
+
+    public ArrayList<Person> getPersons() {
+        return persons;
+    }
 
     public void addUnique(Person newPerson) {
-        boolean isNotUnique = false;
+        boolean isUnique = true;
         for (Person person: persons) {
-            if (person.email.equals(newPerson.email)) {
-                isNotUnique = true;
+            if (person.equals(newPerson)) {
+                isUnique = false;
                 break;
             }
         }
-        if (!isNotUnique){
+        if (isUnique){
             persons.add(newPerson);
         }
 
     }
-
 
 
 //    public class String Search () {

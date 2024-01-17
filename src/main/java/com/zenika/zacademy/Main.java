4c2f1f34b697e48b0676a64a518fd14187fb0891
Lisa Daudibon <lisa.daudibon@outlook.fr>
2024-01-17 10:56:36 +0100

    :sparkles: LD adding the first data for Person  with faker

--- src/main/java/com/zenika/zacademy/Main.java
+++ src/main/java/com/zenika/zacademy/Main.java
@@ -2,17 +2,28 @@ package com.zenika.zacademy;
 
 import net.datafaker.Faker;
 
+import java.util.Iterator;
 import java.util.Locale;
+import java.util.List;
 
 public class Main {
-    Faker faker = new Faker(new Locale("fr"));
+    public static void main(String[] args) {
+        Faker faker = new Faker(new Locale("fr"));
 
-    String firstName = faker.name().firstName();
-    String lastName = faker.name().lastName();
-    String phoneNumber = faker.phoneNumber().phoneNumber();
-    String email = faker.internet().emailAddress();
-    String adress = faker.address().fullAddress();
+        String firstName = faker.name().firstName();
+        String lastName = faker.name().lastName();
+        String phoneNumber = faker.phoneNumber().phoneNumber();
+        String email = faker.internet().emailAddress();
+        String adress = faker.address().fullAddress();
 
-    Person Maximilien = new Student(firstName, lastName, phoneNumber, email, adress);
+        Person maximilien = new Student(firstName, lastName, phoneNumber, email, adress);
+
+        Directory zacademyDirectory = new Directory();
+
+        zacademyDirectory.addUnique(maximilien);
+
+//        Iterator iter = zacademyDirectory.getPersons().iterator();
+
+    }
 
 }
